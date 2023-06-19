df89fdb5aaea96f72b8991277bf9b6efbcbc7797
sayed.masud <sayed.masud@leads-bd.com>
2023-06-19 14:09:32 +0600

    recycler list added

--- api/src/main/java/com/example/capita/instrument/Instrument.kt
+++ api/src/main/java/com/example/capita/instrument/Instrument.kt
@@ -1,4 +1,13 @@
 package com.example.capita.instrument
 
-class Instrument(val id: Int, val name: String, val symbol:String, val market: String, val assetClass: String) {
-}
+//class Instrument(val id: Int, val name: String, val symbol:String, val market: String, val assetClass: String) {
+//}
+class Instrument( val icon: Int,
+                       val longName:String,
+                       val shortName: String,
+                       val equity: String,
+                       val market: String,
+                       val value: Double,
+                       val closedPrice: Double,
+                       val change: Double,
+                       val changeIcon: Int)
--- app/src/main/java/com/example/capita/home/topBarScreen/InstrumentView.kt
+++ app/src/main/java/com/example/capita/home/topBarScreen/InstrumentView.kt
@@ -1,33 +1,104 @@
 package com.example.capita.home.topBarScreen
 
-import androidx.cardview.widget.CardView
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import com.example.capita.instrument.Instrument
 
 @Composable
 fun CardView(instrument: Instrument) {
     Card(
-        shape = RoundedCornerShape(8.dp),
+        modifier = Modifier.padding(16.dp),
         elevation = 8.dp,
-        modifier = Modifier
-            .padding(8.dp)
-            .fillMaxWidth()
+        backgroundColor = Color(0xFFFFFFFF),
+        shape = MaterialTheme.shapes.medium
     ) {
-        Column(modifier = Modifier.padding(16.dp)) {
-            Text(text = instrument.id.toString(), modifier = Modifier.padding(8.dp))
-            Text(text = instrument.name, modifier = Modifier.padding(8.dp))
-            Text(text = instrument.symbol, modifier = Modifier.padding(8.dp))
-            Text(text = instrument.market, modifier = Modifier.padding(8.dp))
-            Text(text = instrument.assetClass, modifier = Modifier.padding(8.dp))
-        }
+        Row(modifier = Modifier.padding(8.dp)) {
+            // First Column
+            Column(modifier = Modifier.weight(4f)) {
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    // First Row - Icon and Full Name
+                    Image(
+                        painter = painterResource(id = instrument.icon),
+                        contentDescription = null,
+                        modifier = Modifier.size(32.dp)
+                    )
+                    Text(
+                        text = instrument.longName,
+                        modifier = Modifier.padding(start = 8.dp),
+                        style = MaterialTheme.typography.subtitle1
+                    )
+                }
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    // Second Row - Short Name, Equity, Market
+                    Text(
+                        text = instrument.shortName,
+                        style = MaterialTheme.typography.body1
+                    )
+                    Text("|", modifier = Modifier.padding(horizontal = 4.dp))
+                    Text(
+                        text = instrument.equity,
+                        style = MaterialTheme.typography.body1
+                    )
+                    Text("|", modifier = Modifier.padding(horizontal = 4.dp))
+                    Text(
+                        text = instrument.market,
+                        style = MaterialTheme.typography.body1
+                    )
+                }
+            }
+            // Second Column
+            Column(modifier = Modifier.weight(2f)) {
+                Column(modifier = Modifier.fillMaxHeight(), verticalArrangement = Arrangement.Center) {
+                    // First Row - Value
+                    Text(
+                        text = instrument.value.toString(),
+                        style = MaterialTheme.typography.subtitle1,
+                        modifier = Modifier.padding(vertical = 4.dp)
+                    )
+                }
+                Column(modifier = Modifier.fillMaxHeight().align(Alignment.CenterHorizontally), verticalArrangement = Arrangement.Center) {
+                    // Second Row - Closed Price and Change
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Image(
+                            painter = painterResource(id = instrument.changeIcon),
+                            contentDescription = null,
+                            modifier = Modifier.size(16.dp)
+                        )
+                        Text(
+                            text = instrument.closedPrice.toString(),
+                            style = MaterialTheme.typography.body1,
+                        )
+                        Text("(")
+                        if(instrument.change.toString()=="-"){
+                            Image(
+                                painter = painterResource(id = instrument.changeIcon),
+                                contentDescription = null,
+                                modifier = Modifier.size(16.dp)
+                            )
+                        }
 
+
+                        Text(
+                            text = instrument.change.toString(),
+                            style = MaterialTheme.typography.body1,
+                        )
+                        Text(")")
+
+                    }
+                }
+            }
+        }
     }
 }
